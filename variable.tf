8e7a6a68fb2c75366fba3254250fd5e8fdf8fd32
baka126 <117905389+baka126@users.noreply.github.com>
2024-01-11 10:10:08 +0545

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,48 +1,144 @@
-variable "web_acl_name" {
-  description = "Name for the AWS WAF WebACL"
+# Web ACL Configuration Variables
+variable "name" {
   type        = string
+  description = "A friendly name of the WebACL."
+}
+
+variable "scope" {
+  type        = string
+  description = "The scope of this Web ACL. Valid options: CLOUDFRONT, REGIONAL."
 }
 
 variable "default_action" {
-  description = "Default action for the WebACL"
   type        = string
-  default     = "ALLOW" # You can customize the default value as needed
+  description = "The action to perform if none of the rules contained in the WebACL match."
+  default     = "allow"
 }
 
-variable "priority" {
-  description = "Priority for the WebACL rules"
-  type        = number
-  default     = 1 # You can customize the default value as needed
+# Managed Rules Configuration
+variable "managed_rules" {
+  type        = list(object({
+    name                 = string
+    priority             = number
+    override_action      = string
+    vendor_name          = string
+    version              = optional(string)
+    rule_action_override = list(object({
+      name          = string
+      action_to_use = string
+    }))
+  }))
+  description = "List of Managed WAF rules."
+  default = [
+    {
+      name                 = "AWSManagedRulesCommonRuleSet"
+      # ... other default values
+    },
+    # ... additional managed rules
+  ]
 }
 
-variable "rule_action" {
-  description = "Action to be taken when a rule is matched"
+# IP Set Rule Configuration
+variable "ip_sets_rule" {
+  type        = list(object({
+    name          = string
+    priority      = number
+    ip_set_arn    = string
+    action        = string
+    response_code = optional(number, 403)
+  }))
+  description = "A rule to detect web requests coming from particular IP addresses or address ranges."
+  default     = []
+}
+
+# IP Rate-Based Rule Configuration
+variable "ip_rate_based_rule" {
+  type        = object({
+    name          = string
+    priority      = number
+    limit         = number
+    action        = string
+    response_code = optional(number, 403)
+  })
+  description = "A rate-based rule tracks the rate of requests for each originating IP address."
+  default     = null
+}
+
+# IP Rate URL-Based Rules Configuration
+variable "ip_rate_url_based_rules" {
+  type        = list(object({
+    name                  = string
+    priority              = number
+    limit                 = number
+    action                = string
+    response_code         = optional(number, 403)
+    search_string         = string
+    positional_constraint = string
+  }))
+  description = "A rate and URL-based rule tracks the rate of requests for each originating IP address."
+  default     = []
+}
+
+# Filtered Header Rule Configuration
+variable "filtered_header_rule" {
+  type        = object({
+    header_types  = list(string)
+    priority      = number
+    header_value  = string
+    action        = string
+    search_string = string
+  })
+  description = "HTTP header to filter."
+  default = {
+    header_types  = []
+    priority      = 1
+    header_value  = ""
+    action        = "block"
+    search_string = ""
+  }
+}
+
+# Logging Configuration Variables
+variable "enable_logging" {
+  type        = bool
+  description = "Whether to associate Logging resource with the WAFv2 ACL."
+  default     = false
+}
+
+variable "log_destination_arns" {
+  type        = list(string)
+  description = "The ARNs of resources for logging."
+  default     = []
+}
+
+# Association Variables
+variable "associate_alb" {
+  type        = bool
+  description = "Whether to associate an ALB with the WAFv2 ACL."
+  default     = false
+}
+
+variable "alb_arn" {
   type        = string
-  default     = "BLOCK" # You can customize the default value as needed
+  description = "ARN of the ALB to be associated with the WAFv2 ACL."
+  default     = ""
 }
 
-variable "rule_priority" {
-  description = "Priority for the WAF rule"
-  type        = number
-  default     = 1 # You can customize the default value as needed
+# Rule Group Configuration
+variable "group_rules" {
+  type        = list(object({
+    name            = string
+    arn             = string
+    priority        = number
+    override_action = string
+  }))
+  description = "List of WAFv2 Rule Groups."
+  default     = []
 }
 
-variable "rule_name" {
-  description = "Name for the WAF rule"
-  type        = string
-}
-
-variable "statement" {
-  description = "Statement for the WAF rule"
-  type        = list(any)
-}
-
-variable "resource_type" {
-  description = "Type of resource (alb or cloudfront)"
-  type        = string
-}
-
-variable "resource_arn" {
-  description = "ARN of the resource to associate with the WebACL"
-  type        = string
+# Tags Configuration
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags to assign to the WAFv2 ACL."
+  default     = {}
 }
