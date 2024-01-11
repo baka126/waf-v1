b16d4f3a23c4d9eaac5f31e10c57c67dda1a67ea
baka126 <117905389+baka126@users.noreply.github.com>
2024-01-11 10:09:51 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,261 @@
+# This Terraform module configures AWS WAFv2 Web ACL for protecting resources behind an Application Load Balancer (ALB).
+# It includes settings for managed rules, IP sets, rate-based rules, header filtering, and rule groups.
 
-provider "aws" {
-  # Ensure you have the appropriate AWS credentials and region configured
-  region = "your-aws-region"
-}
+resource "aws_wafv2_web_acl" "main" {
+  name        = var.name
+  description = "WAFv2 ACL for ${var.name}"
 
-resource "aws_wafv2_web_acl" "web_acl" {
-  name        = var.web_acl_name
-  scope       = "CLOUDFRONT" # Use "REGIONAL" for ALB, "CLOUDFRONT" for CloudFront
-  description = "AWS WAF WebACL for protecting the application"
+  scope = var.scope
+
+  # Default Action Settings
   default_action {
-    allow {}
-  }
-}
-
-resource "aws_wafv2_web_acl_association" "web_acl_association" {
-  resource_arn = var.resource_arn
-  web_acl_arn  = aws_wafv2_web_acl.web_acl.arn
-}
-
-resource "aws_wafv2_rule_group" "rule_group" {
-  name        = "your-rule-group-name"
-  scope       = var.resource_type == "alb" ? "REGIONAL" : "CLOUDFRONT"
-  description = "Rule group for AWS WAF"
-  capacity    = 100
-  rules {
-    priority    = var.rule_priority
-    name        = var.rule_name
-    action {
-      block {}
+    dynamic "allow" {
+      for_each = var.default_action == "allow" ? [1] : []
+      content {}
     }
-    statement {
-      or_statement {
-        statements = var.statement
+
+    dynamic "block" {
+      for_each = var.default_action == "block" ? [1] : []
+      content {}
+    }
+  }
+
+  # Visibility Configuration
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    sampled_requests_enabled   = true
+    metric_name                = var.name
+  }
+
+  # Managed Rules Configuration
+  dynamic "rule" {
+    for_each = var.managed_rules
+    content {
+      # Rule Settings
+      name     = rule.value.name
+      priority = rule.value.priority
+
+      # Override Action Settings
+      override_action {
+        dynamic "none" {
+          for_each = rule.value.override_action == "none" ? [1] : []
+          content {}
+        }
+
+        dynamic "count" {
+          for_each = rule.value.override_action == "count" ? [1] : []
+          content {}
+        }
+      }
+
+      # Statement for Managed Rule Group
+      statement {
+        managed_rule_group_statement {
+          name        = rule.value.name
+          vendor_name = rule.value.vendor_name
+          version     = rule.value.version
+
+          # Rule Action Override Settings
+          dynamic "rule_action_override" {
+            for_each = rule.value.rule_action_override
+            content {
+              name = rule_action_override.value["name"]
+              
+              # Action to Use Settings
+              dynamic "allow" {
+                for_each = rule_action_override.value["action_to_use"] == "allow" ? [1] : []
+                content {}
+              }
+
+              dynamic "block" {
+                for_each = rule_action_override.value["action_to_use"] == "block" ? [1] : []
+                content {}
+              }
+
+              dynamic "count" {
+                for_each = rule_action_override.value["action_to_use"] == "count" ? [1] : []
+                content {}
+              }
+            }
+          }
+        }
+      }
+
+      # Visibility Configuration for the Rule
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.name
+        sampled_requests_enabled   = true
       }
     }
   }
-}
 
-resource "aws_wafv2_web_acl_rule_group_insertion" "rule_group_insertion" {
-  name        = aws_wafv2_web_acl.web_acl.name
-  priority    = var.priority
-  rule_action {
-    block {}
+  # IP Set Rules Configuration
+  dynamic "rule" {
+    for_each = var.ip_sets_rule
+    content {
+      # Rule Settings
+      name     = rule.value.name
+      priority = rule.value.priority
+
+      # Action Settings
+      action {
+        dynamic "allow" {
+          for_each = rule.value.action == "allow" ? [1] : []
+          content {}
+        }
+
+        dynamic "count" {
+          for_each = rule.value.action == "count" ? [1] : []
+          content {}
+        }
+
+        dynamic "block" {
+          for_each = rule.value.action == "block" ? [1] : []
+          content {
+            # Custom Response for Block Action
+            dynamic "custom_response" {
+              for_each = rule.value.response_code != 403 ? [1] : []
+              content {
+                response_code = rule.value.response_code
+              }
+            }
+          }
+        }
+      }
+
+      # Statement for IP Set Reference
+      statement {
+        ip_set_reference_statement {
+          arn = rule.value.ip_set_arn
+        }
+      }
+
+      # Visibility Configuration for the Rule
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.name
+        sampled_requests_enabled   = true
+      }
+    }
   }
-  web_acl_arn = aws_wafv2_web_acl.web_acl.arn
-  rule_group_reference_statement {
-    name = aws_wafv2_rule_group.rule_group.name
+
+  # Rate-Based IP Rule Configuration
+  dynamic "rule" {
+    for_each = var.ip_rate_based_rule != null ? [var.ip_rate_based_rule] : []
+    content {
+      # Rule Settings
+      name     = rule.value.name
+      priority = rule.value.priority
+
+      # Action Settings
+      action {
+        dynamic "count" {
+          for_each = rule.value.action == "count" ? [1] : []
+          content {}
+        }
+
+        dynamic "block" {
+          for_each = rule.value.action == "block" ? [1] : []
+          content {
+            # Custom Response for Block Action
+            dynamic "custom_response" {
+              for_each = rule.value.response_code != 403 ? [1] : []
+              content {
+                response_code = rule.value.response_code
+              }
+            }
+          }
+        }
+      }
+
+      # Statement for Rate-Based IP
+      statement {
+        rate_based_statement {
+          limit              = rule.value.limit
+          aggregate_key_type = "IP"
+        }
+      }
+
+      # Visibility Configuration for the Rule
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.name
+        sampled_requests_enabled   = true
+      }
+    }
   }
-}
+
+  # Rate-Based URL Rule Configuration
+  dynamic "rule" {
+    for_each = var.ip_rate_url_based_rules
+    content {
+      # Rule Settings
+      name     = rule.value.name
+      priority = rule.value.priority
+
+      # Action Settings
+      action {
+        dynamic "allow" {
+          for_each = rule.value.action == "allow" ? [1] : []
+          content {}
+        }
+
+        dynamic "count" {
+          for_each = rule.value.action == "count" ? [1] : []
+          content {}
+        }
+
+        dynamic "block" {
+          for_each = rule.value.action == "block" ? [1] : []
+          content {
+            # Custom Response for Block Action
+            dynamic "custom_response" {
+              for_each = rule.value.response_code != 403 ? [1] : []
+              content {
+                response_code = rule.value.response_code
+              }
+            }
+          }
+        }
+      }
+
+      # Statement for Rate-Based URL
+      statement {
+        rate_based_statement {
+          limit              = rule.value.limit
+          aggregate_key_type = "IP"
+          
+          # Scope Down Statement for URL Match
+          scope_down_statement {
+            byte_match_statement {
+              positional_constraint = rule.value.positional_constraint
+              search_string         = rule.value.search_string
+              field_to_match {
+                uri_path {}
+              }
+              text_transformation {
+                priority = 0
+                type     = "URL_DECODE"
+              }
+            }
+          }
+        }
+      }
+
+      # Visibility Configuration for the Rule
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.name
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  # Filtered Header Rule Configuration
+  dynamic "rule" {
+    for_each = [for header_name in var.filtered_header_rule.header_types : {
+      priority      = var.filtered_header_rule.priority + index(var.filtered_header_rule.header_types, header_name) + 1
+      name          = header_name
+      header_value  = var.filtered_header_rule.header_value
+      action        = var.filtered_header_rule
